42a854dcc7fdb2515bbdb6373abc1a12556110de
Franklin van de Meent <fr@nkl.in>
2013-08-06 21:39:04 +0200

    Created geoip2ws.js

---
+++ geoip2ws.js
@@ -0,0 +1,7 @@
+/*
+Name:         geoip2ws
+Description:  Maxmind GeoIP2 Web Services for Node.js
+Source:       https://github.com/fvdm/nodejs-geoip2ws
+Feedback:     https://github.com/fvdm/nodejs-geoip2ws/issues
+License:      Public Domain / Unlicense
+*/
