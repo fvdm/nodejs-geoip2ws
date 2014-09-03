27a4a43f5b54b8a9efc49fede43a8b7668386c97
Franklin van de Meent <fr@nkl.in>
2014-09-04 00:14:27 +0200

    Tests: clean up whitespace

--- test.js
+++ test.js
@@ -13,13 +13,13 @@ var geo = require('./')(
 // handle exits
 var errors = 0
 process.on( 'exit', function() {
-	if( errors == 0 ) {
-		console.log('\n\033[1mDONE, no errors.\033[0m\n')
-		process.exit(0)
-	} else {
-		console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n')
-		process.exit(1)
-	}
+  if( errors == 0 ) {
+    console.log('\n\033[1mDONE, no errors.\033[0m\n')
+    process.exit(0)
+  } else {
+    console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n')
+    process.exit(1)
+  }
 })
 
 // prevent errors from killing the process
@@ -30,40 +30,40 @@ var queue = []
 var next = 0
 
 function doNext() {
-	next++
-	if( queue[next] ) {
-		queue[next]()
-	}
+  next++
+  if( queue[next] ) {
+    queue[next]()
+  }
 }
 
 // doTest( passErr, 'methods', [
 //   ['feeds', typeof feeds === 'object']
 // ])
 function doTest( err, label, tests ) {
-	if( err instanceof Error ) {
-		console.error( label +': \033[1m\033[31mERROR\033[0m\n' )
-		console.error( util.inspect(err, false, 10, true) )
-		console.log()
-		console.error( err.stack )
-		console.log()
-		errors++
-	} else {
-		var testErrors = []
-		tests.forEach( function( test ) {
-			if( test[1] !== true ) {
-				testErrors.push(test[0])
-				errors++
-			}
-		})
-		
-		if( testErrors.length == 0 ) {
-			console.log( label +': \033[1mok\033[0m' )
-		} else {
-			console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' )
-		}
-	}
-	
-	doNext()
+  if( err instanceof Error ) {
+    console.error( label +': \033[1m\033[31mERROR\033[0m\n' )
+    console.error( util.inspect(err, false, 10, true) )
+    console.log()
+    console.error( err.stack )
+    console.log()
+    errors++
+  } else {
+    var testErrors = []
+    tests.forEach( function( test ) {
+      if( test[1] !== true ) {
+        testErrors.push(test[0])
+        errors++
+      }
+    })
+
+    if( testErrors.length == 0 ) {
+      console.log( label +': \033[1mok\033[0m' )
+    } else {
+      console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' )
+    }
+  }
+
+  doNext()
 }
 
 // METHOD
