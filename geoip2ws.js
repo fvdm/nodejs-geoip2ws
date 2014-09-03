62b090021763dae51649f958a394a4c62e57eb4d
Franklin van de Meent <fr@nkl.in>
2014-09-04 00:14:03 +0200

    Clean up whitespace

--- geoip2ws.js
+++ geoip2ws.js
@@ -13,127 +13,127 @@ var https = require('https')
 
 // setup
 var app = {
-	userId: null,
-	licenseKey: null,
-	service: 'city',
-	requestTimeout: 5000
+  userId: null,
+  licenseKey: null,
+  service: 'city',
+  requestTimeout: 5000
 }
 
 module.exports = function( userId, licenseKey, service, requestTimeout ) {
-	if( userId === undefined || licenseKey === undefined ) {
-		return new Error('no userId or licenseKey')
-	}
-	
-	app.userId = userId
-	app.licenseKey = licenseKey
-	
-	if( typeof service === 'number' ) {
-		app.requestTimeout = service
-	} else {
-		app.service = service || app.service
-		app.requestTimeout = requestTimeout || app.requestTimeout
-	}
-	
-	return function( service, ip, callback ) {
-		
-		// service is optional
-		if( arguments.length === 2 ) {
-			var callback = ip
-			var ip = service
-			var service = app.service
-		}
-		
-		// prevent multiple callbacks
-		var complete = false
-		function doCallback( err, res ) {
-			if( ! complete ) {
-				complete = true
-				callback( err, res )
-			}
-		}
-		
-		// build request
-		var options = {
-			hostname: 'geoip.maxmind.com',
-			path: '/geoip/v2.1/'+ service +'/'+ ip,
-			agent: false,
-			headers: {
-				'Accept': 'application/json',
-				'User-Agent': 'https://github.com/fvdm/nodejs-geoip2ws'
-			},
-			auth: app.userId +':'+ app.licenseKey
-		}
-		
-		var request = https.request( options )
-		
-		// request response
-		request.on( 'response', function( response ) {
-			var data = []
-			var size = 0
-			var err = null
-			
-			// collect data
-			response.on( 'data', function( chunk ) {
-				data.push( chunk )
-				size += chunk.length
-			})
-			
-			// process data
-			response.on( 'end', function() {
-				if( data.length >= 1 ) {
-					var buf = new Buffer( size )
-					var pos = 0
-					
-					for( var i=0; i<data.length; i++ ) {
-						data[i].copy( buf, pos )
-						pos += data[i].length
-					}
-					
-					data = data.toString('utf8').trim()
-					
-					try {
-						data = JSON.parse( data )
-						
-						if( data.error !== undefined ) {
-							err = new Error('API error')
-							err.code = data.code
-							err.error = data.error
-						}
-					} catch(e) {
-						err = new Error('not json')
-					}
-				} else {
-					err = new Error('no data')
-				}
-				
-				doCallback( err, data )
-			})
-			
-			// connection dropped
-			response.on( 'close', function() {
-				doCallback( new Error('request dropped') )
-			})
-		})
-		
-		request.on( 'socket', function( socket ) {
-			if( app.requestTimeout !== undefined ) {
-				socket.setTimeout( app.requestTimeout )
-				socket.on( 'timeout', function() {
-					request.abort()
-					doCallback( new Error('request timeout') )
-				})
-			}
-		})
-		
-		// request error
-		request.on( 'error', function( error ) {
-			var err = new Error('request failed')
-			err.error = error
-			doCallback( err )
-		})
-		
-		// request finished
-		request.end()
-		
-	}
+  if( userId === undefined || licenseKey === undefined ) {
+    return new Error('no userId or licenseKey')
+  }
+
+  app.userId = userId
+  app.licenseKey = licenseKey
+
+  if( typeof service === 'number' ) {
+    app.requestTimeout = service
+  } else {
+    app.service = service || app.service
+    app.requestTimeout = requestTimeout || app.requestTimeout
+  }
+
+  return function( service, ip, callback ) {
+
+    // service is optional
+    if( arguments.length === 2 ) {
+      var callback = ip
+      var ip = service
+      var service = app.service
+    }
+
+    // prevent multiple callbacks
+    var complete = false
+    function doCallback( err, res ) {
+      if( ! complete ) {
+        complete = true
+        callback( err, res )
+      }
+    }
+
+    // build request
+    var options = {
+      hostname: 'geoip.maxmind.com',
+      path: '/geoip/v2.1/'+ service +'/'+ ip,
+      agent: false,
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'https://github.com/fvdm/nodejs-geoip2ws'
+      },
+      auth: app.userId +':'+ app.licenseKey
+    }
+
+    var request = https.request( options )
+
+    // request response
+    request.on( 'response', function( response ) {
+      var data = []
+      var size = 0
+      var err = null
+
+      // collect data
+      response.on( 'data', function( chunk ) {
+        data.push( chunk )
+        size += chunk.length
+      })
+
+      // process data
+      response.on( 'end', function() {
+        if( data.length >= 1 ) {
+          var buf = new Buffer( size )
+          var pos = 0
+
+          for( var i=0; i<data.length; i++ ) {
+            data[i].copy( buf, pos )
+            pos += data[i].length
+          }
+
+          data = data.toString('utf8').trim()
+
+          try {
+            data = JSON.parse( data )
+
+            if( data.error !== undefined ) {
+              err = new Error('API error')
+              err.code = data.code
+              err.error = data.error
+            }
+          } catch(e) {
+            err = new Error('not json')
+          }
+        } else {
+          err = new Error('no data')
+        }
+
+        doCallback( err, data )
+      })
+
+      // connection dropped
+      response.on( 'close', function() {
+        doCallback( new Error('request dropped') )
+      })
+    })
+
+    request.on( 'socket', function( socket ) {
+      if( app.requestTimeout !== undefined ) {
+        socket.setTimeout( app.requestTimeout )
+        socket.on( 'timeout', function() {
+          request.abort()
+          doCallback( new Error('request timeout') )
+        })
+      }
+    })
+
+    // request error
+    request.on( 'error', function( error ) {
+      var err = new Error('request failed')
+      err.error = error
+      doCallback( err )
+    })
+
+    // request finished
+    request.end()
+
+  }
 }
