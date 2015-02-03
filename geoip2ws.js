02344fee564ecd40d7e15da276d491c71dcee8fd
Franklin van de Meent <fr@nkl.in>
2015-02-03 14:26:00 +0100

    Add semicolons (#5)

--- geoip2ws.js
+++ geoip2ws.js
@@ -9,8 +9,8 @@ This product includes GeoLite2 data created by MaxMind, available from
 <http://www.maxmind.com>.
 */
 
-var https = require('https')
-var net = require('net')
+var https = require('https');
+var net = require('net');
 
 // setup
 var app = {
@@ -18,51 +18,51 @@ var app = {
   licenseKey: null,
   service: 'city',
   requestTimeout: 5000
-}
+};
 
 module.exports = function( userId, licenseKey, service, requestTimeout ) {
   if( userId === undefined || licenseKey === undefined ) {
-    return new Error('no userId or licenseKey')
+    return new Error('no userId or licenseKey');
   }
 
-  app.userId = userId
-  app.licenseKey = licenseKey
+  app.userId = userId;
+  app.licenseKey = licenseKey;
 
   if( typeof service === 'number' ) {
-    app.requestTimeout = service
+    app.requestTimeout = service;
   } else {
-    app.service = service || app.service
-    app.requestTimeout = requestTimeout || app.requestTimeout
+    app.service = service || app.service;
+    app.requestTimeout = requestTimeout || app.requestTimeout;
   }
 
   return function( service, ip, callback ) {
-    serviceRegexp = new RegExp(/^(country|city|insights)$/)
+    serviceRegexp = new RegExp(/^(country|city|insights)$/);
 
     // service is optional
     if( typeof service === 'string' && ! service.match( serviceRegexp ) ) {
-      var callback = ip
-      var ip = service
-      var service = app.service
+      var callback = ip;
+      var ip = service;
+      var service = app.service;
     }
 
     // prevent multiple callbacks
-    var complete = false
+    var complete = false;
     function doCallback( err, res ) {
       if( ! complete ) {
-        complete = true
-        callback( err, res )
+        complete = true;
+        callback( err, res );
       }
     }
 
     // check input
     if( ! service.match( serviceRegexp ) ) {
-      doCallback( new Error('invalid service') )
-      return
+      doCallback( new Error('invalid service') );
+      return;
     }
     
     if( ! net.isIP( ip ) ) {
-      doCallback( new Error('invalid ip') )
-      return
+      doCallback( new Error('invalid ip') );
+      return;
     }
     
     // build request
@@ -75,70 +75,70 @@ module.exports = function( userId, licenseKey, service, requestTimeout ) {
         'User-Agent': 'geoip2ws.js'
       },
       auth: app.userId +':'+ app.licenseKey
-    }
+    };
 
-    var request = https.request( options )
+    var request = https.request( options );
 
     // request response
     request.on( 'response', function( response ) {
-      var data = []
-      var size = 0
-      var err = null
+      var data = [];
+      var size = 0;
+      var err = null;
 
       // collect data
       response.on( 'data', function( chunk ) {
-        data.push( chunk )
-        size += chunk.length
-      })
+        data.push( chunk );
+        size += chunk.length;
+      });
 
       // process data
       response.on( 'end', function() {
         if( data.length >= 1 ) {
-          data = new Buffer.concat( data, size )
-          data = data.toString('utf8').trim()
+          data = new Buffer.concat( data, size );
+          data = data.toString('utf8').trim();
 
           try {
-            data = JSON.parse( data )
+            data = JSON.parse( data );
 
             if( data.error !== undefined ) {
-              err = new Error('API error')
-              err.code = data.code
-              err.error = data.error
+              err = new Error('API error');
+              err.code = data.code;
+              err.error = data.error;
             }
           } catch(e) {
-            err = new Error('not json')
+            err = new Error('not json');
           }
         } else {
-          err = new Error('no data')
+          err = new Error('no data');
         }
 
-        doCallback( err, data )
-      })
+        doCallback( err, data );
+      });
 
       // connection dropped
       response.on( 'close', function() {
-        doCallback( new Error('request dropped') )
-      })
-    })
+        doCallback( new Error('request dropped') );
+      });
+    });
 
     request.on( 'socket', function( socket ) {
       if( app.requestTimeout ) {
-        socket.setTimeout( app.requestTimeout )
+        socket.setTimeout( app.requestTimeout );
         socket.on( 'timeout', function() {
-          doCallback( new Error('request timeout') )
-          request.abort()
-        })
+          doCallback( new Error('request timeout') );
+          request.abort();
+        });
       }
-    })
+    });
 
     // request error
     request.on( 'error', function( error ) {
-      var err = new Error('request failed')
-      err.error = error
-      doCallback( err )
-    })
+      var err = new Error('request failed');
+      err.error = error;
+      doCallback( err );
+    });
 
     // request finished
-    request.end()
+    request.end();
   }
 }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var util = require('util')
+var util = require('util');
 
 // Setup
 // set env GEOIP2WS_USERID and GEOIP2WS_LICENSE (Travis CI)
@@ -7,38 +7,38 @@ var geo = require('./')(
   process.env.npm_config_app_userid || process.env.GEOIP2WS_USERID || null,
   process.env.npm_config_app_license || process.env.GEOIP2WS_LICENSE || null,
   process.env.npm_config_app_service || process.env.GEOIP2WS_SERVICE || 'city'
-)
+);
 
 
 // handle exits
 var errors = 0
 process.on( 'exit', function() {
   if( errors == 0 ) {
-    console.log('\n\033[1mDONE, no errors.\033[0m\n')
-    process.exit(0)
+    console.log('\n\033[1mDONE, no errors.\033[0m\n');
+    process.exit(0);
   } else {
-    console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n')
-    process.exit(1)
+    console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n');
+    process.exit(1);
   }
-})
+});
 
 // prevent errors from killing the process
 process.on( 'uncaughtException', function( err ) {
-  console.log()
-  console.error( err.stack )
-  console.trace()
-  console.log()
-  errors++
-})
+  console.log();
+  console.error( err.stack );
+  console.trace();
+  console.log();
+  errors++;
+});
 
 // Queue to prevent flooding
-var queue = []
-var next = 0
+var queue = [];
+var next = 0;
 
 function doNext() {
-  next++
+  next++;
   if( queue[next] ) {
-    queue[next]()
+    queue[next]();
   }
 }
 
@@ -47,37 +47,37 @@ function doNext() {
 // ])
 function doTest( err, label, tests ) {
   if( err instanceof Error ) {
-    console.error( label +': \033[1m\033[31mERROR\033[0m\n' )
-    console.error( util.inspect(err, false, 10, true) )
-    console.log()
-    console.error( err.stack )
-    console.log()
-    errors++
+    console.error( label +': \033[1m\033[31mERROR\033[0m\n' );
+    console.error( util.inspect(err, false, 10, true) );
+    console.log();
+    console.error( err.stack );
+    console.log();
+    errors++;
   } else {
-    var testErrors = []
+    var testErrors = [];
     tests.forEach( function( test ) {
       if( test[1] !== true ) {
-        testErrors.push(test[0])
-        errors++
+        testErrors.push(test[0]);
+        errors++;
       }
-    })
+    });
 
     if( testErrors.length == 0 ) {
-      console.log( label +': \033[1m\033[32mok\033[0m' )
+      console.log( label +': \033[1m\033[32mok\033[0m' );
     } else {
-      console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' )
+      console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' );
     }
   }
 
-  doNext()
+  doNext();
 }
 
 // METHOD
 queue.push( function() {
   doTest( null, 'module', [
     ['function type', typeof geo === 'function']
-  ])
-})
+  ]);
+});
 
 // Test error
 queue.push( function() {
@@ -85,18 +85,18 @@ queue.push( function() {
     doTest( null, 'error', [
       ['type', err instanceof Error],
       ['message', err.message === 'invalid ip']
-    ])
-  })
-})
+    ]);
+  });
+});
 
 // Test success
 queue.push( function() {
   geo('74.125.206.100', function(err, data) {
     doTest( err, 'lookup', [
       ['city name', !err && data && typeof data.city.names.en === 'string']
-    ])
-  })
-})
+    ]);
+  });
+});
 
 // Start the tests
-queue[0]()
+queue[0]();
