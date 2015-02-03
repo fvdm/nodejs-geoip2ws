196a850d27726ece779a01d140295bcc0e76dadf
Franklin van de Meent <fr@nkl.in>
2015-02-03 14:33:46 +0100

    Clean up code style

--- geoip2ws.js
+++ geoip2ws.js
@@ -9,8 +9,8 @@ This product includes GeoLite2 data created by MaxMind, available from
 <http://www.maxmind.com>.
 */
 
-var https = require('https');
-var net = require('net');
+var https = require ('https');
+var net = require ('net');
 
 // setup
 var app = {
@@ -20,26 +20,26 @@ var app = {
   requestTimeout: 5000
 };
 
-module.exports = function( userId, licenseKey, service, requestTimeout ) {
-  if( userId === undefined || licenseKey === undefined ) {
-    return new Error('no userId or licenseKey');
+module.exports = function (userId, licenseKey, service, requestTimeout) {
+  if (userId === undefined || licenseKey === undefined) {
+    return new Error ('no userId or licenseKey');
   }
 
   app.userId = userId;
   app.licenseKey = licenseKey;
 
-  if( typeof service === 'number' ) {
+  if (typeof service === 'number') {
     app.requestTimeout = service;
   } else {
     app.service = service || app.service;
     app.requestTimeout = requestTimeout || app.requestTimeout;
   }
 
-  return function( service, ip, callback ) {
-    serviceRegexp = new RegExp(/^(country|city|insights)$/);
+  return function (service, ip, callback) {
+    serviceRegexp = new RegExp (/^ (country|city|insights)$/);
 
     // service is optional
-    if( typeof service === 'string' && ! service.match( serviceRegexp ) ) {
+    if (typeof service === 'string' && ! service.match (serviceRegexp) ) {
       var callback = ip;
       var ip = service;
       var service = app.service;
@@ -47,21 +47,21 @@ module.exports = function( userId, licenseKey, service, requestTimeout ) {
 
     // prevent multiple callbacks
     var complete = false;
-    function doCallback( err, res ) {
-      if( ! complete ) {
+    function doCallback (err, res) {
+      if (! complete) {
         complete = true;
-        callback( err, res );
+        callback (err, res);
       }
     }
 
     // check input
-    if( ! service.match( serviceRegexp ) ) {
-      doCallback( new Error('invalid service') );
+    if (! service.match (serviceRegexp) ) {
+      doCallback (new Error ('invalid service'));
       return;
     }
     
-    if( ! net.isIP( ip ) ) {
-      doCallback( new Error('invalid ip') );
+    if (! net.isIP (ip) ) {
+      doCallback (new Error ('invalid ip'));
       return;
     }
     
@@ -77,68 +77,68 @@ module.exports = function( userId, licenseKey, service, requestTimeout ) {
       auth: app.userId +':'+ app.licenseKey
     };
 
-    var request = https.request( options );
+    var request = https.request (options);
 
     // request response
-    request.on( 'response', function( response ) {
+    request.on ('response', function (response) {
       var data = [];
       var size = 0;
       var err = null;
 
       // collect data
-      response.on( 'data', function( chunk ) {
-        data.push( chunk );
+      response.on ('data', function (chunk) {
+        data.push (chunk);
         size += chunk.length;
       });
 
       // process data
-      response.on( 'end', function() {
-        if( data.length >= 1 ) {
-          data = new Buffer.concat( data, size );
-          data = data.toString('utf8').trim();
+      response.on ('end', function () {
+        if (data.length >= 1) {
+          data = new Buffer.concat (data, size);
+          data = data.toString ('utf8').trim ();
 
           try {
-            data = JSON.parse( data );
+            data = JSON.parse (data);
 
-            if( data.error !== undefined ) {
-              err = new Error('API error');
+            if (data.error !== undefined) {
+              err = new Error ('API error');
               err.code = data.code;
               err.error = data.error;
             }
-          } catch(e) {
-            err = new Error('not json');
+          } catch (e) {
+            err = new Error ('not json');
           }
         } else {
-          err = new Error('no data');
+          err = new Error ('no data');
         }
 
-        doCallback( err, data );
+        doCallback (err, data);
       });
 
       // connection dropped
-      response.on( 'close', function() {
-        doCallback( new Error('request dropped') );
+      response.on ('close', function () {
+        doCallback (new Error ('request dropped'));
       });
     });
 
-    request.on( 'socket', function( socket ) {
-      if( app.requestTimeout ) {
-        socket.setTimeout( app.requestTimeout );
-        socket.on( 'timeout', function() {
-          doCallback( new Error('request timeout') );
-          request.abort();
+    request.on ('socket', function (socket) {
+      if (app.requestTimeout) {
+        socket.setTimeout (app.requestTimeout);
+        socket.on ('timeout', function () {
+          doCallback (new Error ('request timeout'));
+          request.abort ();
         });
       }
     });
 
     // request error
-    request.on( 'error', function( error ) {
-      var err = new Error('request failed');
+    request.on ('error', function (error) {
+      var err = new Error ('request failed');
       err.error = error;
-      doCallback( err );
+      doCallback (err);
     });
 
     // request finished
-    request.end();
+    request.end ();
   }
 }
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var util = require('util');
+var util = require ('util');
 
 // Setup
-// set env GEOIP2WS_USERID and GEOIP2WS_LICENSE (Travis CI)
+// set env GEOIP2WS_USERID and GEOIP2WS_LICENSE  (Travis CI)
 // or user cli arguments: npm test --app_userid=1234 --app_license=abc123
-var geo = require('./')(
+var geo = require ('./') (
   process.env.npm_config_app_userid || process.env.GEOIP2WS_USERID || null,
   process.env.npm_config_app_license || process.env.GEOIP2WS_LICENSE || null,
   process.env.npm_config_app_service || process.env.GEOIP2WS_SERVICE || 'city'
@@ -12,22 +12,22 @@ var geo = require('./')(
 
 // handle exits
 var errors = 0
-process.on( 'exit', function() {
-  if( errors == 0 ) {
-    console.log('\n\033[1mDONE, no errors.\033[0m\n');
-    process.exit(0);
+process.on ('exit', function () {
+  if (errors == 0) {
+    console.log ('\n\033[1mDONE, no errors.\033[0m\n');
+    process.exit (0);
   } else {
-    console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n');
-    process.exit(1);
+    console.log ('\n\033[1mFAIL, '+ errors +' error'+  (errors > 1 ? 's' : '') +' occurred!\033[0m\n');
+    process.exit (1);
   }
 });
 
 // prevent errors from killing the process
-process.on( 'uncaughtException', function( err ) {
-  console.log();
-  console.error( err.stack );
-  console.trace();
-  console.log();
+process.on ('uncaughtException', function (err) {
+  console.log ();
+  console.error (err.stack);
+  console.trace ();
+  console.log ();
   errors++;
 });
 
@@ -35,54 +35,54 @@ process.on( 'uncaughtException', function( err ) {
 var queue = [];
 var next = 0;
 
-function doNext() {
+function doNext () {
   next++;
-  if( queue[next] ) {
-    queue[next]();
+  if (queue[next]) {
+    queue[next] ();
   }
 }
 
-// doTest( passErr, 'methods', [
+// doTest (passErr, 'methods', [
 //   ['feeds', typeof feeds === 'object']
 // ])
-function doTest( err, label, tests ) {
-  if( err instanceof Error ) {
-    console.error( label +': \033[1m\033[31mERROR\033[0m\n' );
-    console.error( util.inspect(err, false, 10, true) );
-    console.log();
-    console.error( err.stack );
-    console.log();
+function doTest (err, label, tests) {
+  if (err instanceof Error) {
+    console.error (label +': \033[1m\033[31mERROR\033[0m\n');
+    console.error (util.inspect (err, false, 10, true));
+    console.log ();
+    console.error (err.stack);
+    console.log ();
     errors++;
   } else {
     var testErrors = [];
-    tests.forEach( function( test ) {
-      if( test[1] !== true ) {
-        testErrors.push(test[0]);
+    tests.forEach (function (test) {
+      if (test[1] !== true) {
+        testErrors.push (test[0]);
         errors++;
       }
     });
 
-    if( testErrors.length == 0 ) {
-      console.log( label +': \033[1m\033[32mok\033[0m' );
+    if (testErrors.length == 0) {
+      console.log (label +': \033[1m\033[32mok\033[0m');
     } else {
-      console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' );
+      console.error (label +': \033[1m\033[31mfailed\033[0m  ('+ testErrors.join (', ') +')');
     }
   }
 
-  doNext();
+  doNext ();
 }
 
 // METHOD
-queue.push( function() {
-  doTest( null, 'module', [
+queue.push (function () {
+  doTest (null, 'module', [
     ['function type', typeof geo === 'function']
   ]);
 });
 
 // Test error
-queue.push( function() {
-  geo('invalid input', function(err, data) {
-    doTest( null, 'error', [
+queue.push (function () {
+  geo ('invalid input', function (err, data) {
+    doTest (null, 'error', [
       ['type', err instanceof Error],
       ['message', err.message === 'invalid ip']
     ]);
@@ -90,13 +90,13 @@ queue.push( function() {
 });
 
 // Test success
-queue.push( function() {
-  geo('74.125.206.100', function(err, data) {
-    doTest( err, 'lookup', [
+queue.push (function () {
+  geo ('74.125.206.100', function (err, data) {
+    doTest (err, 'lookup', [
       ['city name', !err && data && typeof data.city.names.en === 'string']
     ]);
   });
 });
 
 // Start the tests
-queue[0]();
+queue[0] ();
