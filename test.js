6f4eef632e8c8d874f450a12737304c3463243fc
Franklin van de Meent <fr@nkl.in>
2017-12-12 13:11:27 +0100

    Test(main): Convert syntax to ES6

--- test.js
+++ test.js
@@ -7,15 +7,15 @@ Feedback:       https://github.com/fvdm/nodejs-geoip2ws/issues
 License:        Unlicense (public domain, see LICENSE file)
 */
 
-var doTest = require ('dotest');
-var app = require ('./');
+const doTest = require ('dotest');
+const app = require ('./');
 
-var geo;
+let geo;
 
 
 // Setup
 // set env GEOIP2WS_USERID and GEOIP2WS_LICENSE  (CI tests)
-var config = {
+const config = {
   userId: process.env.GEOIP2WS_USERID || null,
   licenseKey: process.env.GEOIP2WS_LICENSE || null,
   service: process.env.GEOIP2WS_SERVICE || 'city',
@@ -35,9 +35,9 @@ var config = {
  */
 
 function checkSuccess (test, err, data, ret) {
-  var names = data && data.city && data.city.names;
-  var dataIP = data && data.traits && data.traits.ip_address;
-  var dataSub = data && data.most_specific_subdivision.iso_code;
+  const names = data && data.city && data.city.names;
+  const dataIP = data && data.traits && data.traits.ip_address;
+  const dataSub = data && data.most_specific_subdivision.iso_code;
 
   test (err)
     .isObject ('fail', 'data', data)
@@ -48,103 +48,103 @@ function checkSuccess (test, err, data, ret) {
     .isExactly ('fail', 'data.most_specific_subdivision', dataSub, 'CA');
 
   if (!err && typeof ret !== 'undefined') {
-    test ()
+    test()
       .isFunction ('fail', 'return', ret);
   }
 
-  test ()
-    .done ();
+  test()
+    .done();
 }
 
 
 // TESTS
-doTest.add ('Configuration', function (test) {
+doTest.add ('Configuration'.test => {
   if (!config.userId || !config.licenseKey) {
     config.endpoint = 'https://frankl.in/u/ci_test.php?a=geoip2ws&b=';
 
-    test ()
+    test()
       .warn ('GEOIP2WS_USERID or GEOIP2WS_LICENSE not set')
       .info ('Using test endpoint with fake data');
   }
 
   geo = app (config);
 
-  test ()
+  test()
     .info ('config.service:         ' + config.service)
     .info ('config.requestTimeout:  ' + config.requestTimeout)
-    .done ();
+    .done();
 });
 
 
 // Module
-doTest.add ('Module', function (test) {
-  test ()
+doTest.add ('Module',test => {
+  test()
     .isFunction ('fail', 'exports', app)
     .isFunction ('fail', 'interface', geo)
-    .done ();
+    .done();
 });
 
 
 // Test success
-doTest.add ('lookup - arguments', function (test) {
-  var ret = geo ('74.125.206.100', function (err, data) {
+doTest.add ('lookup - arguments',test => {
+  const ret = geo ('74.125.206.100',(err, data) => {
     checkSuccess (test, err, data, ret);
   });
 });
 
 
-doTest.add ('lookup - object', function (test) {
-  var obj = {
+doTest.add ('lookup - object',test => {
+  const obj = {
     ip: '74.125.206.100',
     service: config.service
   };
 
-  geo (obj, function (err, data) {
+  geo (obj,(err, data) => {
     checkSuccess (test, err, data);
   });
 });
 
 
 // Satellite IPs have no geo location
-doTest.add ('lookup - data.subdivisions array', function (test) {
-  geo ('95.107.128.1', function (err, data) {
-    var dataIP = data && data.traits && data.traits.ip_address;
+doTest.add ('lookup - data.subdivisions array',test => {
+  geo ('95.107.128.1',(err, data) => {
+    const dataIP = data && data.traits && data.traits.ip_address;
 
     test (err)
       .isArray ('fail', 'data.subdivisions', data.subdivisions)
       .isEmpty ('fail', 'data.subdivisions', data.subdivisions)
       .isExactly ('fail', 'data.traits.ip_address', dataIP, '95.107.128.1')
-      .done ();
+      .done();
   });
 });
 
 
 // Test errors
-doTest.add ('Error: invalid ip', function (test) {
-  geo ('invalid input', function (err, data) {
-    test ()
+doTest.add ('Error: invalid ip',test => {
+  geo ('invalid input',(err, data) => {
+    test()
       .isError ('fail', 'err', err)
       .isExactly ('fail', 'err.message', err && err.message, 'invalid ip')
       .isUndefined ('fail', 'data', data)
-      .done ();
+      .done();
   });
 });
 
 
-doTest.add ('Error: invalid service', function (test) {
-  geo ('invalid service', '74.125.206.100', function (err, data) {
-    test ()
+doTest.add ('Error: invalid service',test => {
+  geo ('invalid service', '74.125.206.100',(err, data) => {
+    test()
       .isError ('fail', 'err', err)
       .isExactly ('fail', 'err.message', err && err.message, 'invalid service')
       .isUndefined ('fail', 'data', data)
-      .done ();
+      .done();
   });
 });
 
 
-doTest.add ('Error: API error', function (test) {
-  geo ('0.0.0.0', function (err, data) {
-    test ()
+doTest.add ('Error: API error',test => {
+  geo ('0.0.0.0',(err, data) => {
+    test()
       .isError ('fail', 'err', err)
       .isExactly ('fail', 'err.message', err && err.message, 'API error')
       .isString ('fail', 'err.code', err && err.code)
@@ -152,34 +152,34 @@ doTest.add ('Error: API error', function (test) {
       .isString ('fail', 'err.error', err && err.error)
       .isNotEmpty ('warn', 'err.error', err && err.error)
       .isUndefined ('fail', 'data', data)
-      .done ();
+      .done();
   });
 });
 
 
-doTest.add ('Setup with arguments', function (test) {
+doTest.add ('Setup with arguments',test => {
   geo = app (config.userId, config.licenseKey, config.service, config.timeout);
 
-  geo ('74.125.206.100', function (err, data) {
+  geo ('74.125.206.100',(err, data) => {
     checkSuccess (test, err, data);
   });
 });
 
 
-doTest.add ('Error: request timeout', function (test) {
-  var tmp = app (config.userId, config.licenseKey, 1);
+doTest.add ('Error: request timeout',test => {
+  const tmp = app (config.userId, config.licenseKey, 1);
 
-  tmp ('74.125.206.100', function (err, data) {
-    test ()
+  tmp ('74.125.206.100',(err, data) => {
+    test()
       .isError ('fail', 'err', err)
       .isExactly ('fail', 'err.message', err && err.message, 'request failed')
       .isError ('fail', 'err.error', err && err.error)
       .isExactly ('fail', 'err.error.code', err && err.error && err.error.code, 'TIMEOUT')
       .isUndefined ('fail', 'data', data)
-      .done ();
+      .done();
   });
 });
 
 
 // Start the tests
-doTest.run ();
+doTest.run();
